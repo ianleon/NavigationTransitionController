0456a4cc7b13f91ddbc69f01c3fb7277f5137c6f
Ian Leon <iannleon@me.com>
2020-12-22 22:12:49 -0500

    Expanded example project

--- Example/NavigationTransitionController/ViewController.swift
+++ Example/NavigationTransitionController/ViewController.swift
@@ -11,14 +11,35 @@ import NavigationTransitionController
 
 
 class ToViewController: UIViewController {
+    let transitionFinal = UIView(frame: .zero)
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        view.addSubview(transitionFinal)
+        
+        transitionFinal.translatesAutoresizingMaskIntoConstraints = false
+        transitionFinal.backgroundColor = .systemRed
+        NSLayoutConstraint.activate([
+            
+            transitionFinal.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            transitionFinal.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+//            transitionFinal.widthAnchor.constraint(equalTo: view.widthAnchor),
+//            transitionFinal.heightAnchor.constraint(equalTo: view.widthAnchor),
+            
+            // Issue here
+            // Dismissing looks fine
+            transitionFinal.widthAnchor.constraint(equalToConstant: 150),
+            transitionFinal.heightAnchor.constraint(equalToConstant: 150)
+        ])
+        
     }
 }
 
 class ViewController: UIViewController {
 
+    let transitionInitial = UIView(frame: .zero)
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray5
@@ -27,18 +48,98 @@ class ViewController: UIViewController {
         tap.numberOfTapsRequired = 1
         view.isUserInteractionEnabled = true
         
-        let zoomButton = UIButton(type: .system)
-        zoomButton.translatesAutoresizingMaskIntoConstraints = false
-        zoomButton.setTitle("Zoom Transition", for: .normal)
+        let listStack = UIStackView(frame: .zero)
+        listStack.alignment = .leading
+        listStack.axis = .vertical
+        listStack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(listStack)
         
-        view.addSubview(zoomButton)
+        listStack.addArrangedSubview(transitionInitial)
+        
+        let makeTrig = {
+            (title: String, action: Selector) -> UIButton in
+            
+            let button = UIButton(type: .system)
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setTitle(title, for: .normal)
+            button.addTarget(self, action: action, for: .touchUpInside)
+
+            return button
+        }
+        
+        for trigger in [
+            
+            makeTrig("Standard", #selector(loadWithStandard)),
+            makeTrig("Presentation", #selector(loadWithPresentation)),
+            makeTrig("Zoom", #selector(loadWithZoom)),
+            makeTrig("Fade", #selector(loadWithFade))
+        ] {
+            listStack.addArrangedSubview(trigger)
+        }
+        
+        transitionInitial.translatesAutoresizingMaskIntoConstraints = false
+        transitionInitial.backgroundColor = .systemRed
         
         NSLayoutConstraint.activate([
-            zoomButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
-            zoomButton.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 16)
+            transitionInitial.widthAnchor.constraint(equalToConstant: 44),
+            transitionInitial.heightAnchor.constraint(equalToConstant: 44),
+            listStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            listStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16)
         ])
+
         
-        zoomButton.addTarget(<#T##target: Any?##Any?#>, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+    }
+    
+    @objc func loadWithZoom() {
+        
+        let toVC = ToViewController()
+        let nvtc = NavigationTransitionController(
+            rootViewController: toVC,
+            type: .zoom,
+            initialView: transitionInitial,
+            finalView: toVC.transitionFinal
+        )
+        // TODO cannot be dismissed by swiping up
+        // Probably because swiping up in the app is used for comments
+        
+        nvtc.presentNavigation(self)
+    }
+    
+    @objc func loadWithFade() {
+        
+        let toVC = ToViewController()
+        let nvtc = NavigationTransitionController(
+            rootViewController: toVC,
+            type: .fade
+        )
+        nvtc.presentNavigation(self)
+    }
+    
+    @objc func loadWithPresentation() {
+        
+        let toVC = ToViewController()
+        let nvtc = NavigationTransitionController(
+            rootViewController: toVC,
+            type: .presentation
+        )
+        // NOTE this transition cannot be dismissed with a gesture
+        nvtc.presentNavigation(self)
+    }
+    
+    
+    @objc func loadWithStandard() {
+        
+        let toVC = ToViewController()
+        let nvtc = NavigationTransitionController(
+            rootViewController: toVC,
+            type: .standard,
+            initialView: transitionInitial,
+            finalView: toVC.transitionFinal
+        )
+        // NOTE disimissing requires a flick
+        // Swiping all the way to the end and letting go will not trigger dismiss
+        nvtc.presentNavigation(self)
     }
 
     @objc fileprivate func loadToVC(_ sender: Any) {
