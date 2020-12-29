65bc3527ce36ae4e5c924d5d0a21bec84f61f8fa
Ian Leon <iannleon@me.com>
2020-12-28 23:20:58 -0500

    Cleans up ViewController

--- Example/NavigationTransitionController/ViewController.swift
+++ Example/NavigationTransitionController/ViewController.swift
@@ -21,78 +21,55 @@ class ToViewController: UIViewController {
         transitionFinal.translatesAutoresizingMaskIntoConstraints = false
         transitionFinal.backgroundColor = .systemRed
         NSLayoutConstraint.activate([
-            
             transitionFinal.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             transitionFinal.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-//            transitionFinal.widthAnchor.constraint(equalTo: view.widthAnchor),
-//            transitionFinal.heightAnchor.constraint(equalTo: view.widthAnchor),
-            
-            // Issue here
-            // Dismissing looks fine
-            transitionFinal.widthAnchor.constraint(equalToConstant: 150),
-            transitionFinal.heightAnchor.constraint(equalToConstant: 150)
+            transitionFinal.widthAnchor.constraint(equalTo: view.widthAnchor),
+            transitionFinal.heightAnchor.constraint(equalTo: view.widthAnchor),
         ])
-        
     }
 }
 
 class ViewController: UIViewController {
 
-    let transitionInitial = UIView(frame: .zero)
+    var transitionInitial:UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+
         view.backgroundColor = .systemGray5
-
-        let tap = UITapGestureRecognizer(target: self, action: #selector(loadToVC(_:)))
-        tap.numberOfTapsRequired = 1
-        view.isUserInteractionEnabled = true
         
-        let listStack = UIStackView(frame: .zero)
-        listStack.alignment = .leading
-        listStack.axis = .vertical
-        listStack.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(listStack)
-        
-        listStack.addArrangedSubview(transitionInitial)
-        
-        let makeTrig = {
-            (title: String, action: Selector) -> UIButton in
-            
-            let button = UIButton(type: .system)
-            
-            button.translatesAutoresizingMaskIntoConstraints = false
-            button.setTitle(title, for: .normal)
-            button.addTarget(self, action: action, for: .touchUpInside)
-
-            return button
-        }
-        
-        for trigger in [
-            
-            makeTrig("Standard", #selector(loadWithStandard)),
-            makeTrig("Presentation", #selector(loadWithPresentation)),
-            makeTrig("Zoom", #selector(loadWithZoom)),
-            makeTrig("Fade", #selector(loadWithFade))
-        ] {
-            listStack.addArrangedSubview(trigger)
-        }
-        
-        transitionInitial.translatesAutoresizingMaskIntoConstraints = false
+        // Set up view to transition
+        transitionInitial = UIView(frame: .init(x: 8, y: 70, width: 44, height: 44))
         transitionInitial.backgroundColor = .systemRed
-        
-        NSLayoutConstraint.activate([
-            transitionInitial.widthAnchor.constraint(equalToConstant: 44),
-            transitionInitial.heightAnchor.constraint(equalToConstant: 44),
-            listStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
-            listStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16)
-        ])
+        view.addSubview(transitionInitial)
 
         
+        // Set up transition buttons
+        let buttonStack = UIStackView(
+            arrangedSubviews:
+                [
+                    ("Standard", #selector(loadWithStandard)),
+                    ("Presentation", #selector(loadWithPresentation)),
+                    ("Zoom", #selector(loadWithZoom)),
+                    ("Fade", #selector(loadWithFade))
+                ].map {
+                    (title,action) -> UIButton in
+            
+                    let button = UIButton(type: .system)
+                    
+                    button.setTitle(title, for: .normal)
+                    button.addTarget(self, action: action, for: .touchUpInside)
+
+                    return button
+                }
+        )
+        buttonStack.alignment = .leading
+        buttonStack.distribution = .equalSpacing
+        buttonStack.axis = .vertical
+        buttonStack.frame = .init(x: 8, y: 140, width: 500, height: 150)
+        view.addSubview(buttonStack)
     }
     
     @objc func loadWithZoom() {
-        
         let toVC = ToViewController()
         let nvtc = NavigationTransitionController(
             rootViewController: toVC,
@@ -100,14 +77,10 @@ class ViewController: UIViewController {
             initialView: transitionInitial,
             finalView: toVC.transitionFinal
         )
-        // TODO cannot be dismissed by swiping up
-        // Probably because swiping up in the app is used for comments
-        
         nvtc.presentNavigation(self)
     }
     
     @objc func loadWithFade() {
-        
         let toVC = ToViewController()
         let nvtc = NavigationTransitionController(
             rootViewController: toVC,
@@ -117,19 +90,16 @@ class ViewController: UIViewController {
     }
     
     @objc func loadWithPresentation() {
-        
         let toVC = ToViewController()
         let nvtc = NavigationTransitionController(
             rootViewController: toVC,
             type: .presentation
         )
-        // NOTE this transition cannot be dismissed with a gesture
         nvtc.presentNavigation(self)
     }
     
     
     @objc func loadWithStandard() {
-        
         let toVC = ToViewController()
         let nvtc = NavigationTransitionController(
             rootViewController: toVC,
@@ -137,8 +107,6 @@ class ViewController: UIViewController {
             initialView: transitionInitial,
             finalView: toVC.transitionFinal
         )
-        // NOTE disimissing requires a flick
-        // Swiping all the way to the end and letting go will not trigger dismiss
         nvtc.presentNavigation(self)
     }
 
